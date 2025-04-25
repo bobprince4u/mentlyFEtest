5d252ce7eb127bdeeca23c4d872a1510c1be3690
Bobprince4u <153783664+bobprince4u@users.noreply.github.com>
2025-04-25 11:46:43 +0100

    modified:   components/AccessibilityWrapper.tsx modified:   components/Dashboard.tsx

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -105,7 +105,7 @@ export default function Dashboard() {
               <UsersSection />
             </AnimatedSection>
           
-            <AnimatedSection className="grid justify-end" variant={slideUp} delay={0.6}>
+            <AnimatedSection className="" variant={slideUp} delay={0.6}>
               <RecentActivitiesSection />
             </AnimatedSection>
           
